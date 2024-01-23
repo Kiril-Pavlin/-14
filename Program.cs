23f7f27769b69b322323d5f0a0b6f7856e2dff4a
zxc <zxc@DESKTOP-DFP9Q4Q>
2024-01-23 22:39:23 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компьютерный_практикум___14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // Задание 1. Вывести на экран в столбик первые 10 натуральных чисел (циклы с условием, цикл с параметром)
+            //int x = 1;
+            // while (x <= 10)
+            // {
+            //     Console.WriteLine(x);
+            //     x++;
+            //  }
+
+            // Задание 2.Вывести на экран целые числа из интервала от 1 до n(цикл с параметром)
+            //  Console.WriteLine("Введите число- ");
+            //  int n = int.Parse(Console.ReadLine());
+            //  for (int x= 1; x <= n; x++) 
+            //  {
+            //      Console.WriteLine(x);         
+            //   }
+            //  Задание 3.Вывести в столбик 5 раз слово «Привет!» (циклы с условием)
+            //  int x = 1;
+            //  while (x<=5)
+            //  {
+            //     Console.WriteLine("Привет!");
+            //     x++;
+            //  }
+            // Задание 4.С использованием цветового оформления консоли:
+            //  а) вывести на экран горизонтальную строку из 18 символов;
+            //  б) вывести на экран вертикальную строку из 25 символов
+            //А
+            //   for ( int x = 1; x <=18; x++)
+            //     {
+            //        Console.WriteLine(',');
+            //    }
+            // б
+            // for (int x = 1; x <=25; x++)
+           // {
+                //        Console.WriteLine(',');
+                //    }
+             //   Console.ReadKey();
+          
+
+
+
+
+       }
+    } 
+}
